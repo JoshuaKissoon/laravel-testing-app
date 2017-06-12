5578e686a4cf667c766db8b84b629ccd65b69917
Joshua Kissoon <JoshuaKissoon@gmail.com>
2017-06-12 12:36:02 -0400

    Initial Commit
    Setup a Dusk Browser Test

---
+++ tests/Browser/ButtonClickTest.php
@@ -0,0 +1,29 @@
+<?php
+
+    namespace Tests\Browser;
+
+    use Tests\DuskTestCase;
+    use Laravel\Dusk\Browser;
+    use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+    class ButtonClickTest extends DuskTestCase
+    {
+
+        /**
+         * A Dusk test example.
+         *
+         * @return void
+         */
+        public function testExample()
+        {
+            $this->browse(function (Browser $browser)
+            {
+                $browser->visit('/')
+                        ->clickLink('Click Me')
+                        ->assertSee("You've been clicked, punk.")
+                        ->assertPathIs('/click-me');
+            });
+        }
+
+    }
+    
